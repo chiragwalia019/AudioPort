407f425035fd4253a1a97eedd5f88770446de05d
chiragwalia019 <chiragwalia0@gmail.com>
2023-06-17 01:27:25 +0530

    commit

--- audioport/components/HeroBanner.jsx
+++ audioport/components/HeroBanner.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Link from 'next/link'
+import { urlFor } from '../lib/client'
 const HeroBanner = ({heroBanner}) => {
   return (
     <div className="hero-banner-container">
@@ -7,13 +8,13 @@ const HeroBanner = ({heroBanner}) => {
         <p className='beats-solo'>{heroBanner.smallText}</p>
         <h3>{heroBanner.midText}</h3>
         <h1>{heroBanner.largeText1}</h1>
-        <img src="" alt="headphones" className="hero-banner-image"/>
-        <Link href="/product/ID">
+        <img src={urlFor(heroBanner.image)} alt="headphones" className="hero-banner-image"/>
+        <Link href={`/product/${heroBanner.product}`}>
           <button type="button">{heroBanner.buttonText}</button>
         </Link>
         <div className="desc">
           <h5>Description</h5>
-          <p>DESCRIPTION</p>
+          <p>{heroBanner.desc}</p>
         </div>
       </div>
     </div>

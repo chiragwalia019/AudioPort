5da9e9975b2ac9e1253a8069436dd1de60484622
chiragwalia019 <chiragwalia0@gmail.com>
2023-06-19 14:51:21 +0530

    commit

--- audioport/pages/success.js
+++ audioport/pages/success.js
@@ -26,8 +26,8 @@ const Success = () => {
         <p className="email-msg">Check your email inbox for the receipt.</p>
         <p className="description">
           If you have any questions, please email
-          <a className="email" href="AudioPort1@gmail.com">
-            order@example.com
+          <a className="email" href="mailto:Audioport1@gmail.com">
+          Audioport1@gmail.com
           </a>
         </p>
         <Link href="/">
